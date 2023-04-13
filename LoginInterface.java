b58e078e5809a31de594e750a3f5e421c7925812
Aniketh Jana <96902730+AnikethJana@users.noreply.github.com>
2023-04-13 23:15:22 +0530

    fixed infinite loop
    try again password infinite loop fixed. added counter

--- LoginInterface.java
+++ LoginInterface.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 class LoginInterface 
 {
     public String EntryInterface() 
-    {   
+    {   int i = 1;
         System.out.print("Enter Your UserID : ");
         Scanner sc = new Scanner(System.in);
         String UserID = sc.nextLine();
@@ -15,26 +15,38 @@ class LoginInterface
         //login Interface for Doctor
         if (UserID.equals("DOC001")) {
             
-            while (!Pass.equals("12345678")) {
-                System.out.println("Try Your Password Again");
+            while (!Pass.equals("12345678") && i < 4) {
+                System.out.println("Try Your Password Again - attempt " + i);
                 Pass = sc.nextLine();
+                i++;
             }
-            System.out.println("Welcome Doctor"); 
-           
+            if(Pass.equals("12345678")){
+            System.out.println("Password accepted Entering..."); 
+           //ERROR#################################################
             Map<String,String> hs = PatientInfo.hs;
             DoctorInfo c = new DoctorInfo();
             c.doctorInfo(hs);
-            
+            }
+            else {
+                System.out.println("Maximum Attempts Reached !! Going Back to Home ...");
+                i=0;
+            }
         } 
         //login interface for Patient
         else if (UserID.equals("PAT001"))
         {
-           while (!Pass.equals("123456")) {
-                System.out.println("Try Your Password Again ");
+           while (!Pass.equals("123456") && i < 4) {
+                System.out.println("Try Your Password Again - attempt " + i);
                 Pass = sc.nextLine();
+                i++;
             }
+            if (Pass.equals("123456")){
             System.out.println("Password accepted P.");
             PatientInfo.patientDetails();
+            }
+            else{
+                System.out.println("Maximum Attempts Reached !! Going Back to Home ...");
+            }
         }
         else 
         {
